bc8cc3f94475ce832c7777f3900630e246fc1940
carlos <dutto.c.a@gmail.com>
2025-09-27 12:56:31 -0300

    css2

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const pesa=document.getElementById("pesa");
- const barratateral=document. querySelector(".barra-lateral");
+ const barralateral =document. querySelector(".barra-lateral");
  const span=document.querySelectorAll("span");
  pesa.addEventListener("click",()=>{
- barraLateral.classlist.toggle("mini-barra-lateral");
+ barralateral.classlist.toggle("mini-barra-lateral");
  span.classlist.toggle("oculto");
  
 });
