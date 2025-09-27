6c46a9e7e3658b5b4dcd1a61650beb6a2c9073cb
carlos <dutto.c.a@gmail.com>
2025-09-27 12:41:22 -0300

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+const pesa=document.getElementById("pesa");
+ const barratateral=document. querySelector(".barra-lateral");
+ const span=document.querySelectorAll("span");
+ pesa.addEventListener("click",()=>{
+ barraLateral.classlist.toggle("mini-barra-lateral");
+ span.classlist.toggle("oculto");
+ 
+});
